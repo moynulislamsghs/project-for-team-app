eb34a88092bbe6205a15d7e329107a18cd0c618c
Moynul Islam <moynulislamsghs@gmail.com>
2022-04-27 10:59:36 +0600

    just pushed

--- project_app/lib/screen/splash_screen.dart
+++ project_app/lib/screen/splash_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:project_app/widgets/brand_colors.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: bgColor,
+    );
   }
 }
